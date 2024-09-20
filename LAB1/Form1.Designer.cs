a72f12c7a18092f5a2ec8780b650d23bb81afcae
user <user@WIN-78JSRN4FEM4>
2024-09-20 20:09:25 +0700

    Add project files.

---
+++ LAB1/Form1.Designer.cs
@@ -0,0 +1,516 @@
+﻿namespace LAB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            splitContainer1 = new SplitContainer();
+            textBox8 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            panel7 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            textBox9 = new TextBox();
+            textBox7 = new TextBox();
+            panel11 = new Panel();
+            textBox6 = new TextBox();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            textBox5 = new TextBox();
+            panel8 = new Panel();
+            button4 = new Button();
+            panel10 = new Panel();
+            panel9 = new Panel();
+            button3 = new Button();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            panel7.SuspendLayout();
+            panel12.SuspendLayout();
+            panel13.SuspendLayout();
+            panel11.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.Controls.Add(tabControl1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(797, 542);
+            panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(797, 542);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(splitContainer1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(789, 509);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(3, 3);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.RosyBrown;
+            splitContainer1.Panel1.Controls.Add(textBox8);
+            splitContainer1.Panel1.Controls.Add(panel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackColor = Color.Transparent;
+            splitContainer1.Panel2.Controls.Add(label4);
+            splitContainer1.Panel2.Controls.Add(label3);
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Panel2.Controls.Add(textBox4);
+            splitContainer1.Panel2.Controls.Add(textBox3);
+            splitContainer1.Panel2.Controls.Add(textBox2);
+            splitContainer1.Panel2.Controls.Add(textBox1);
+            splitContainer1.Size = new Size(783, 503);
+            splitContainer1.SplitterDistance = 261;
+            splitContainer1.TabIndex = 0;
+            // 
+            // textBox8
+            // 
+            textBox8.Dock = DockStyle.Fill;
+            textBox8.Location = new Point(0, 0);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(261, 432);
+            textBox8.TabIndex = 1;
+            textBox8.Text = "ItemsListBox";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 243);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Description:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 58);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Cost:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Name:";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(27, 20);
+            label1.TabIndex = 4;
+            label1.Text = "ID:";
+            label1.Click += label1_Click_1;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(3, 266);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(512, 234);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(1, 109);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(514, 119);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(84, 51);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(181, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 18);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(181, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(panel7);
+            tabPage2.Controls.Add(panel6);
+            tabPage2.Controls.Add(panel5);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(789, 509);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.Transparent;
+            panel7.Controls.Add(panel12);
+            panel7.Controls.Add(panel11);
+            panel7.Dock = DockStyle.Top;
+            panel7.Location = new Point(291, 3);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(495, 211);
+            panel7.TabIndex = 2;
+            // 
+            // panel12
+            // 
+            panel12.Controls.Add(panel13);
+            panel12.Controls.Add(textBox7);
+            panel12.Dock = DockStyle.Right;
+            panel12.Location = new Point(96, 0);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(399, 76);
+            panel12.TabIndex = 1;
+            // 
+            // panel13
+            // 
+            panel13.Controls.Add(textBox9);
+            panel13.Dock = DockStyle.Left;
+            panel13.Location = new Point(0, 0);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(181, 49);
+            panel13.TabIndex = 1;
+            // 
+            // textBox9
+            // 
+            textBox9.Dock = DockStyle.Bottom;
+            textBox9.Location = new Point(0, 22);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(181, 27);
+            textBox9.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            textBox7.Dock = DockStyle.Bottom;
+            textBox7.Location = new Point(0, 49);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(399, 27);
+            textBox7.TabIndex = 0;
+            // 
+            // panel11
+            // 
+            panel11.BackColor = Color.Transparent;
+            panel11.Controls.Add(textBox6);
+            panel11.Dock = DockStyle.Bottom;
+            panel11.Location = new Point(0, 76);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(495, 135);
+            panel11.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            textBox6.Dock = DockStyle.Right;
+            textBox6.Location = new Point(96, 0);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(399, 135);
+            textBox6.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Transparent;
+            panel6.Controls.Add(button5);
+            panel6.Dock = DockStyle.Bottom;
+            panel6.Location = new Point(291, 220);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(495, 286);
+            panel6.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(textBox5);
+            panel5.Controls.Add(panel8);
+            panel5.Dock = DockStyle.Left;
+            panel5.Location = new Point(3, 3);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(288, 503);
+            panel5.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            textBox5.Dock = DockStyle.Fill;
+            textBox5.Location = new Point(0, 0);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(288, 438);
+            textBox5.TabIndex = 1;
+            textBox5.Text = "CustomersListBox";
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(button4);
+            panel8.Controls.Add(panel10);
+            panel8.Controls.Add(panel9);
+            panel8.Dock = DockStyle.Bottom;
+            panel8.Location = new Point(0, 438);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(288, 65);
+            panel8.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Fill;
+            button4.Location = new Point(89, 0);
+            button4.Name = "button4";
+            button4.Size = new Size(101, 65);
+            button4.TabIndex = 2;
+            button4.Text = "Remove";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // panel10
+            // 
+            panel10.Dock = DockStyle.Right;
+            panel10.Location = new Point(190, 0);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(98, 65);
+            panel10.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            panel9.Controls.Add(button3);
+            panel9.Dock = DockStyle.Left;
+            panel9.Location = new Point(0, 0);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(89, 65);
+            panel9.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Location = new Point(0, 0);
+            button3.Name = "button3";
+            button3.Size = new Size(89, 65);
+            button3.TabIndex = 0;
+            button3.Text = "Add";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 432);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(261, 71);
+            panel2.TabIndex = 0;
+            panel2.Paint += panel2_Paint;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Transparent;
+            panel4.Dock = DockStyle.Right;
+            panel4.Location = new Point(181, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(80, 71);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.Controls.Add(button1);
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(86, 71);
+            panel3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 71);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(86, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(95, 71);
+            button2.TabIndex = 2;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Fill;
+            button5.Location = new Point(0, 0);
+            button5.Name = "button5";
+            button5.Size = new Size(495, 286);
+            button5.TabIndex = 0;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(797, 542);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            panel12.ResumeLayout(false);
+            panel12.PerformLayout();
+            panel13.ResumeLayout(false);
+            panel13.PerformLayout();
+            panel11.ResumeLayout(false);
+            panel11.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel9.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private SplitContainer splitContainer1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Panel panel5;
+        private Panel panel7;
+        private Panel panel6;
+        private Panel panel8;
+        private Button button4;
+        private Panel panel10;
+        private Panel panel9;
+        private Button button3;
+        private TextBox textBox5;
+        private Panel panel11;
+        private TextBox textBox6;
+        private Panel panel12;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Panel panel13;
+        private TextBox textBox9;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel2;
+        private Panel panel4;
+        private Panel panel3;
+        private Button button1;
+        private Button button2;
+        private Button button5;
+    }
+}
---
+++ LAB1/Form1.cs
@@ -0,0 +1,48 @@
+namespace LAB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel12_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
